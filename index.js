576134c96b565c3a144f22cb305de1e75f564cdd
kevin Wairi <kevinwairi@gmail.com>
2023-03-21 12:12:23 +0300

    Completed Asignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+// this code works when you substitute with the object bt cant pass tests 
+// function superbowlWin(array){
+//     if ( array.result=="W") {
+//          return j;
+//     } else {
+//       return undefined;
+//     }
+
+// }
+// array.find(superbowlWin).year;
+// console.log(array.find(superbowlWin).year);
+
+function superbowlWin(j){
+ const win = j.find(({result})=>result=="W");
+ if (win){
+  return win.year;
+ }else{
+  return undefined
+ }
+  }
+
+
+
